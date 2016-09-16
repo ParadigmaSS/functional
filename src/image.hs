5321da37894f7f366e5a1fc12c7e1b2e3a5caf31
João Paulo Busche <joaopaulo.busche@gmail.com>
2016-09-16 01:45:22 -0300

    Record image filter on new file

--- src/image.hs
+++ src/image.hs
@@ -1,16 +1,23 @@
 import Control.Applicative
 import System.IO
+import Data.List
 
 -- Function which gets the path of image
 getImagePath :: FilePath
 getImagePath = "unb.ppm"
+
+getImageFilterPath :: FilePath
+getImageFilterPath = "filterunb.ppm"
 
 -- Function which applies the negative filter on image
 applyNegative :: [Int] -> [Int]
 -- Verify if list is empty
 applyNegative [] = []
 -- Modify the header of list and pass to the tail list
-applyNegative (h:t) = [h - 255] ++ (applyNegative t)
+applyNegative (h:t) = h - 255 : (applyNegative t)
+
+transformListToString :: [Int] -> String
+transformListToString  lista = concat $ map show lista
 
 -- Function its start the program
 main :: IO ()
@@ -21,5 +28,9 @@ main = do
 	let imageData = drop 20 imageFile
 	-- Convert the image data to a list of ints
 	let intList = map read $ words imageData :: [Int]
-	let imageFilter = applyNegative intList
-	putStrLn $ show imageFilter
+	let imageFilterData = applyNegative intList
+	let imageFilterString = words (transformListToString imageFilterData)
+	let filterData = intercalate " " imageFilterString 
+	let imageFilter = header ++ filterData
+	imageFilterFile <- writeFile getImageFilterPath imageFilter
+	putStrLn $ show imageFilterString
