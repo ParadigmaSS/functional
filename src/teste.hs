bfbc0dead1e6cc32bc144f88bcbc0096f32896d6
ArthurTemporim <arthurrtl@gmail.com>
2016-09-16 00:56:28 -0300

    Program thats open and write an image.

---
+++ src/teste.hs
@@ -0,0 +1,25 @@
+import System.IO
+
+-- Function which applies the negative filter on image                      
+applyNegative :: [Int] -> [Int]
+-- Verify if list is empty
+applyNegative [] = []
+-- Modify the header of list and pass to the tail list
+applyNegative (h:t) = [h - 255] ++ (applyNegative t)
+
+main :: IO ()
+main = do 
+       inh <- openFile "unb.ppm" ReadMode
+       outh <- openFile "saida.ppm" WriteMode
+       mainloop inh outh
+       hClose inh
+       hClose outh
+
+mainloop :: Handle -> Handle -> IO ()
+mainloop inh outh = 
+    do ineof <- hIsEOF inh
+       if ineof
+           then return ()
+           else do inpStr <- hGetLine inh
+                   hPutStrLn outh inpStr
+                   mainloop inh outh
